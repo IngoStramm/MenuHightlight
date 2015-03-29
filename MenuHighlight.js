09648537de2967463bd1798d8666e93b646f35ab
Ingo Stramm <ingostramm@gmail.com>
2015-03-28 22:19:57 -0300

    Commit Inicial
    Javascript/jQuery para destacar a página atual em um menu de navegação,
    usando como referência a URL da página carregada no navegador.

---
+++ MenuHighlight.js
@@ -0,0 +1,27 @@
+﻿/*
+*
+* Construção HTML do menu:
+* ul > li.active > a
+*
+* Referência: http://www.paulund.co.uk/use-jquery-to-highlight-active-menu-item
+*
+*/
+$(document).ready(function(){
+
+    var url = window.location.href;
+
+	// Array com os selectors dos menus
+	var menus = ['#menu-1 a','#menu-2 a'];
+
+	for (var i = menus.length - 1; i >= 0; i--) {
+		$(menus[i]).filter(function () {
+		    return this.href == url;
+		}).parent('li').addClass('active');
+	};
+
+	// Marca os links com a classe '.home' se for a Homepage
+	if(window.location.origin + '/' == url) {
+	    $('li.home').addClass('active');
+	}
+
+}); // $(document).ready(
